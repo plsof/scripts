8d651fd7648febfbf580ecfbb7548601e649f150
pdd <plsof@pdds-MacBook-Pro.local>
2017-07-11 12:37:52 +0800

    syntax error

--- log/tms-login.sh
+++ log/tms-login.sh
@@ -16,4 +16,4 @@ fi
 TIME="["${PRETIME}"+08:00]"
 
 # 时间 HTTP状态码 串号 节点
-awk -v actime=$TIME '$4 >= actime {print $4,$8,substr($12,2,15),$15}' $LOG | awk '{if($3==015164026498581) print "徐总1在线",$0; else if($3==015164026490197) print "徐总2在线"; else if($3==015164026496744) print "夏总在线",$0; else if($3==015164026498133) print "候总在线",$0; else if($3==015164026495932) print "高伟在线",$0; else($3==015373001004176) print "杜鹏在线",$0}'
+awk -v actime=$TIME '$4 >= actime {print $4,$8,substr($12,2,15),$15}' $LOG | awk '{if($3==015164026498581) print "徐总1在线",$0; else if($3==015164026490197) print "徐总2在线"; else if($3==015164026496744) print "夏总在线",$0; else if($3==015164026498133) print "候总在线",$0; else if($3==015164026495932) print "高伟在线",$0; else if($3==015373001004176) print "杜鹏在线",$0}'
