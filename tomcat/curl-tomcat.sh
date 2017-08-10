fc3216869b0af26298d40690d5e7555809eff5df
pdd <plsof@pdds-MacBook-Pro.local>
2017-08-10 11:13:46 +0800

    curl-tomcat.sh

---
+++ tomcat/curl-tomcat.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# author pdd 2017/8/09
+
+URI="xxx"
+
+tomcats=( \
+10.1.32.13 \
+10.1.32.92 \
+10.1.32.93 \
+10.1.32.21 \
+10.1.32.22 \
+10.1.32.25 \
+10.1.32.28 \
+10.1.32.63 \
+10.1.32.65 \
+10.1.32.66 \
+10.1.32.96 \
+10.1.32.117 \
+10.1.32.118 \
+10.1.32.119 \
+10.1.32.120 )
+
+ports=( 6100 6101 6102 6103 6104 )
+
+:>log
+
+for port in "${ports[@]}"
+do
+    for tomcat in "${tomcats[@]}"
+    do
+        echo "$tomcat:$port" >> log
+        curl --connect-timeout 5 "http://$tomcat:$port/$URI" >> log
+        echo
+        sleep 0.5
+    done
+done
