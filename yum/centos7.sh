ae9e16947fef4692384199ce090cd8d490eb8782
pdd <plsof@pdds-MacBook-Pro.local>
2019-08-21 15:14:05 +0800

    添加修改yum源脚本

---
+++ yum/centos7.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
+yum clean all && yum makecache
